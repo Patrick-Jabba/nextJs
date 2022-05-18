b8cc480dedf4e7b6fe0ec0718806106c1921874f
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-05-18 11:08:44 -0300

    construindo a API

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,14 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
     <div>
         <h1>Home</h1>
-        <Contador />
-    </div>
-    )
-
-    function Contador(){
-        const [contador, setContador] = useState(1);
-
-        function adicionarContador(){
-            setContador (contador + 1);
-        }
-        return (
-            <div>
-                <div>{contador}</div>
-                <button onClick={adicionarContador}>Adicionar</button>
-            </div>
-        )
-    }
+        <Link href="/sobre">
+        <a>Acessar Página Sobre</a>
+        </Link>
+    </div>)
+    
 }
 
 export default Home
